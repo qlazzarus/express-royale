4ccb796a33a4423ff4f120f29c2d7bde764c05ae
monoless <ecst@monolife.co.kr>
2020-05-14 17:03:31 +0900

    adding typescript

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,14 @@
 const mix = require('laravel-mix');
 
+require("laravel-mix-react-typescript-extension");
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+  .webpackConfig({
+    output: {
+      path: path.resolve(__dirname, './public_html/')
+    }
+  })
+*/
+mix.reactTypeScript('resources/ts/index.tsx', 'public/js')
+  .sourceMaps(false, 'source-map')
+  .version();
+    //.sass('resources/sass/app.scss', 'public/css');
