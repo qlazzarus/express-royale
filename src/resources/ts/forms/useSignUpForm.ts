880e97d7f2c244c10c8152e642effccc5849bb24
monoless <ecst@monolife.co.kr>
2020-06-23 16:57:51 +0900

    adding schema-to-yup

--- src/resources/ts/forms/useSignUpForm.ts
+++ src/resources/ts/forms/useSignUpForm.ts
@@ -9,7 +9,7 @@ export type SignUpFormData = {
 	passwordConfirm: string
 }
 
-export const SignUpSchema = yup.object().shape({
+const SignUpSchema = yup.object().shape({
     username: yup.string().matches(/[a-z][a-z0-9_.]+/).required(),
     email: yup.string().email().required(),
     password: yup.string().min(8).required(),
