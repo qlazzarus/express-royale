38bdf9aba512f05c558da1b17203e5c6f990c5de
monoless <ecst@monolife.co.kr>
2021-02-03 23:19:50 +0900

    adding 404 white-noise

--- src/resources/ts/languages/Korean.ts
+++ src/resources/ts/languages/Korean.ts
@@ -34,7 +34,8 @@ enum Korean {
     SIGNIN_FACEBOOK = 'Facebook 으로 로그인',
     SIGNIN_GOOGLE = 'Google 로 로그인',
 
-    NOT_FOUND = '찾을 수 없는 페이지입니다.',
+    NOT_FOUND_CODE = '404',
+    NOT_FOUND_MESSAGE = '찾을 수 없는 페이지입니다.',
     
     THROTTLE_REQUESTS_EXCEPTION = '너무 많은 시도를 하였습니다.',
     COMMON_EXCEPTION = '뭔가 문제가 생겼습니다!'
--- src/resources/ts/pages/NotFound.tsx
+++ src/resources/ts/pages/NotFound.tsx
@@ -1,24 +1,64 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useLayoutEffect, useRef} from 'react';
 import {useTranslation} from 'react-i18next';
+import useDimensions from 'react-cool-dimensions';
 import {AspectRatio, Flex, Heading} from '@chakra-ui/react';
 
 import {ColorCode} from "@/enums";
 
+const noise = (canvas: HTMLCanvasElement, width: number, height: number) => {
+    const context = canvas.getContext('2d');
+    if (!context || !width || !height) {
+        return;
+    }
+
+    const imageData = context.createImageData(width, height);
+    const buffer32 = new Uint32Array(imageData.data.buffer);
+    const bufferLength = buffer32.length;
+
+    for (let i = 0; i < bufferLength; i += 1) {
+        // eslint-disable-next-line no-bitwise
+        buffer32[i] = ((255 * Math.random()) || 0) << 24;
+    }
+
+    context.putImageData(imageData, 0, 0);
+}
+
 export default (): JSX.Element => {
-    const canvasRef = useRef(null);
+    const ref = useRef(null);
     const {t} = useTranslation();
+    const {width, height} = useDimensions({ref});
+
+    useLayoutEffect(() => {
+        const animate = () => {
+            const {current} = ref;
+            // eslint-disable-next-line no-useless-return
+            if (!current) return;
+            noise(current as unknown as HTMLCanvasElement, width, height);
+            requestAnimationFrame(animate);
+        }
+
+        requestAnimationFrame(animate);
+    }, [ref, width, height]);
 
     useEffect(() => {
-        console.log(canvasRef.current);
+        const {current} = ref;
+        // eslint-disable-next-line no-useless-return
+        if (!current) return;
+        noise(current as unknown as HTMLCanvasElement, width, height);
     });
 
     return (
         <Flex py='4' direction='column' justifyContent='center' alignItems='center'>
             <AspectRatio m={4} width='50vw' maxW='sm' ratio={1}>
-                <canvas ref={canvasRef} />
+                <canvas
+                    ref={ref}
+                    style={{
+                        borderRadius: '1em'
+                    }}
+                />
             </AspectRatio>
             <Heading textAlign='center' color={ColorCode.PRIMARY_BOLD}>
-                {t('NOT_FOUND')}
+                {t('NOT_FOUND_MESSAGE')}
             </Heading>
         </Flex>
     );
